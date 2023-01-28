0bf1debf90f83a1b1d4d5f1f5ebc640958518a43
OtabekVaxobov <elot2017@mail.ru>
2023-01-28 14:35:06 +0500

    fix: delete all

--- src/components/layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-import Header from '@/components/layout/Header';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  <Header/>
-  return <>{children}</>;
-}

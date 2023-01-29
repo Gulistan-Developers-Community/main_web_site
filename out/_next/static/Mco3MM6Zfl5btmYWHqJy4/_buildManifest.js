3bbe295f73b97d2124220bec36fbd4d65792155c
Otabek_Vaxobov <elot2017@mail.ru>
2023-01-29 22:28:46 +0500

    fix: delete out folder

--- out/_next/static/Mco3MM6Zfl5btmYWHqJy4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d6e74e0e89af7bdc.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

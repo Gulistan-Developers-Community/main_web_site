532911201d24d93ec4951c50d8eb5206b64eb18e
OtabekVaxobov <elot2017@mail.ru>
2023-02-23 17:44:32 +0500

    feat: delete parse and data conections

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,12 @@
-
-import SEO from '../components/seo';
+import { useState } from "react";
 export default function Signup() {
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  
+
   return (
     <>
-      <SEO />
       <div className="-z-1 flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -11,15 +14,7 @@ export default function Signup() {
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                start your 14-day free trial
-              </a>
-            </p>
+            
           </div>
           <form className="mt-8 space-y-6 -z-1" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
@@ -29,6 +24,8 @@ export default function Signup() {
                   Email address
                 </label>
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={Email}
                   id="email-address"
                   name="email"
                   type="email"
@@ -43,6 +40,8 @@ export default function Signup() {
                   Password
                 </label>
                 <input
+                  value={Password}
+                  onChange={(e) => setPassword(e.target.value)}
                   id="password"
                   name="password"
                   type="password"
@@ -80,6 +79,7 @@ export default function Signup() {
             </div>
             <div>
               <button
+                onClick={() => doUserRegistration()}
                 type="submit"
                 className="group  flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
--- src/services/parse.js
+++
@@ -1,8 +0,0 @@
-import Parse from 'parse';
-
-if (typeof window !== "undefined") {
-  Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_JS_KEY); 
-  Parse.serverURL = 'https://parseapi.back4app.com/';
-}
-
-export default Parse;

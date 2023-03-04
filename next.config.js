c9c24e1d62403046d3798451d2b25b7f30cec4ad
Otabek_Vaxobov <elot2017@mail.ru>
2023-03-04 16:41:22 +0500

    fix: some fixes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 // at .next.config.js
 
 module.exports = {
-    // pre-existing configs
-      env: {
-    //you need to insert BOTH KEYs in order to connect with Parse Server
-        PARSE_APP_ID:'ILog4wQzph0fAapJu2jBU2yo6ZAu0HgWiPlZ5dQ1',
-        PARSE_JS_KEY:'KiSfg8E14bgjDjDfSDVhF3knqP0u9Y0kN7FpK5X3'
-      }
-    }
+  // pre-existing configs
+  env: {},
+};

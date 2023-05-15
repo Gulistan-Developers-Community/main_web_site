1212b5c21749dcdfae259420cbac6dc6d2d7f0f1
OtabekVaxobov <elot2017@mail.ru>
2023-05-15 17:23:55 +0500

    feat: add editing func

--- src/pages/cabinet/editing.js
+++ src/pages/cabinet/editing.js
@@ -11,6 +11,7 @@ import { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
 import Login from '../../components/Login';
 import { useRouter } from 'next/router';
+import React from "react";
 
 export default function EditPage() {
   const auth = getAuth();
@@ -24,14 +25,18 @@ export default function EditPage() {
 
   let router = useRouter();
   const currentUser = useAuth();
-  if (!user) {
-    router.push('/signup');
-  }
-  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/signup");
+    }
+  }, [router, user]);
+
+  const [name, setName] = useState();
   const [photo, setPhoto] = useState(null);
   const [loading, setLoading] = useState(false);
   const [photoURL, setPhotoURL] = useState(
-    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+    "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
   );
   console.log(currentUser);
 
@@ -59,27 +64,62 @@ export default function EditPage() {
     });
 
   useEffect(() => {
+    // @ts-ignore
     if (currentUser?.photoURL) {
+      // @ts-ignore
       setPhotoURL(currentUser.photoURL);
     }
   }, [currentUser]);
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-    formData.firstName = inputFirstName?.current?.value || '';
-    formData.lastName = inputLastName?.current?.value || '';
-    formData.age = inputAge?.current?.value || '';
-    console.log(formData);
-    //Form submission happens here
-  };
 
   return (
     <>
       <div className="flex items-center flex-col justify-center mx-auto max-w-7xl">
         <form className="flex flex-col" onSubmit={null}>
-          <div className="fields">
-            <input type="file" onChange={handleChange} />
-            <button disabled={loading || !photo} onClick={handleClick}>
+          <label
+            htmlFor="email-address-icon"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Name
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              id="email-address-icon"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="John Doe"
+              onChange={(e) =>
+                setName(
+                  // @ts-ignore
+                  e.target.value
+                )
+              }
+            />
+          </div>
+
+          <div className="fields flex flex-col">
+            <label
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="user_avatar"
+            >
+              Upload Profile photo
+            </label>
+            <input
+              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              aria-describedby="user_avatar_help"
+              id="user_avatar"
+              type="file"
+              onChange={handleChange}
+            />
+            <div
+              className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+              id="user_avatar_help"
+            ></div>
+
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              disabled={loading || !photo}
+              onClick={handleClick}
+            >
               Upload
             </button>
             <Image
@@ -91,11 +131,11 @@ export default function EditPage() {
             />
           </div>
         </form>
-        <p>name: {displayName || 'none'}</p>
-        <p>email: {email || 'none'}</p>
-        <p>photoURL: {photoURL1 || 'none'}</p>
-        <p>{emailVerified || 'none'}</p>
-        <p>uid: {uid || 'none'}</p>
+        <p>name: {displayName || "none"}</p>
+        <p>email: {email || "none"}</p>
+        <p>photoURL: {photoURL1 || "none"}</p>
+        <p>{emailVerified || "none"}</p>
+        <p>uid: {uid || "none"}</p>
       </div>
     </>
   );
